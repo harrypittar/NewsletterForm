73c3848989dee770513160905b38d7926e343d42
Trent Lim <trent_lim_nz@yahoo.com>
2021-10-07 18:25:31 +1300

    Add shell commands to make subnet public

--- create-vpc.sh
+++ create-vpc.sh
@@ -10,3 +10,11 @@ aws ec2 create-subnet --vpc-id vpc-013d086fde1b98a32 --cidr-block 10.0.0.0/24 --
 # Create private subnets
 aws ec2 create-subnet --vpc-id vpc-013d086fde1b98a32 --cidr-block 10.0.1.0/24 --availability-zone us-east-1b
 aws ec2 create-subnet --vpc-id vpc-013d086fde1b98a32 --cidr-block 10.0.2.0/24 --availability-zone us-east-1c
+
+# Make subnet public
+aws ec2 create-internet-gateway --query InternetGateway.InternetGatewayId --output text
+aws ec2 attach-internet-gateway --vpc-id vpc-013d086fde1b98a32 --internet-gateway-id igw-0524fe0d55672a5a4
+aws ec2 create-route-table --vpc-id vpc-013d086fde1b98a32 --query RouteTable.RouteTableId --output text
+aws ec2 create-route --route-table-id rtb-03260cc9d8ebf20d2 --destination-cidr-block 0.0.0.0/0 --gateway-id igw-0524fe0d55672a5a4
+aws ec2 describe-subnets --filters "Name=vpc-id,Values=vpc-013d086fde1b98a32" --query "Subnets[*].{ID:SubnetId,CIDR:CidrBlock}"
+aws ec2 associate-route-table  --subnet-id subnet-06b70225232ffec28 --route-table-id rtb-03260cc9d8ebf20d2
