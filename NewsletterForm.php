000ab3e3003cfee4eb1895c47a2356f4f8eeeedf
harrypittar <85140615+harrypittar@users.noreply.github.com>
2021-10-07 15:04:52 +1300

    First commit, skeleton php code with username input

---
+++ NewsletterForm.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Newsletter Registration Form</title>
+
+
+    </head>
+
+    <body>
+
+        <center>
+            <h3>SUBSCRIBE HERE FOR NEWSLETTERS</h3>
+
+            <form action="" method=""
+
+                <table>
+                    <tr>
+                        <td>Username:</td>
+                        <td>
+                            <input type"text" name="user">
+                        </td>
+                    </tr>
+                </table>
+        </center>
+
+
+    </body>
+
+
+
+
+
+</html>
