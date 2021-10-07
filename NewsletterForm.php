8220e29031178ba468d3425960ec330b1100d279
harrypittar <85140615+harrypittar@users.noreply.github.com>
2021-10-07 15:55:02 +1300

    Added data fields

--- NewsletterForm.php
+++ NewsletterForm.php
@@ -11,16 +11,44 @@
         <center>
             <h3>SUBSCRIBE HERE FOR NEWSLETTERS</h3>
 
-            <form action="" method=""
+            <form action="" method="">
 
                 <table>
+                   
+
                     <tr>
-                        <td>Username:</td>
+                        <td>Fullname:</td>
                         <td>
-                            <input type"text" name="user">
+                            <input type="Password" name="User_pass">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Email:</td>
+                        <td>
+                            <input type="email" name="mail">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Gender:</td>
+                        <td>
+                            <input type="radio" name="gender">Male
+                            <input type="radio" name="gender">Female
+                            <input type="radio" name="gender">Others
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Phone:</td>
+                        <td>
+                            <select>
+                                <option>+64</option>
+                                <option>+61</option>
+                            </select>
+                            <input type="phone" name="mob_digits">
                         </td>
                     </tr>
                 </table>
+
+            </form>
         </center>
 
 
