bd8d7460a9de580898dadc7aa0c511c880e3ed91
Trent Lim <trent_lim_nz@yahoo.com>
2021-10-12 18:28:51 +1300

    Fix webserver setup process: set up apache permissions

--- setup-webservers.sh
+++ setup-webservers.sh
@@ -1,5 +1,7 @@
 ssh -i "~/.ssh/aws-keypair.pem" ec2-user@user-webserver.com 'bash -s' < install-apache.sh
+ssh -i "~/.ssh/aws-keypair.pem" ec2-user@user-webserver.com 'bash -s' < update-apache-permissions.sh
 ssh -i "~/.ssh/aws-keypair.pem" ec2-user@admin-webserver.com 'bash -s' < install-apache.sh
+ssh -i "~/.ssh/aws-keypair.pem" ec2-user@admin-webserver.com 'bash -s' < update-apache-permissions.sh
 scp -i "~/.ssh/aws-keypair.pem" dbinfo.inc ec2-user@user-webserver.com:/var/www/inc
 scp -i "~/.ssh/aws-keypair.pem" dbinfo.inc ec2-user@admin-webserver.com:/var/www/inc
 scp -i "~/.ssh/aws-keypair.pem" Form.php ec2-user@user-webserver.com:/var/www/html
